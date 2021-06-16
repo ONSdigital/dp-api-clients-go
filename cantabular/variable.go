b9abb72a0d0906cae8ec10b8b6793e48ac417e24
Joshua Medley <jmedleyx7@gmail.com>
2021-06-16 13:09:03 +0100

    Update client and data structures to use Cantabular v9

--- cantabular/variable.go
+++ cantabular/variable.go
@@ -2,11 +2,17 @@ package cantabular
 
 // Variable represents a 'variable' object returned from Cantabular
 type Variable struct{
-	Name         string   `json:"name"`
-	Label        string   `json:"label"`
-	Len          int      `json:"len"`
-	Codes        []string `json:"codes,omitempty"`
-	Labels       []string `json:"labels,omitempty"`
-	MapFrom      []string `json:"mapFrom,omitempty"`
-	MapFromCodes []string `json:"mapFromCodes,omitempty"`
+	Name         string    `json:"name"`
+	Label        string    `json:"label"`
+	Len          int       `json:"len"`
+	Codes        []string  `json:"codes,omitempty"`
+	Labels       []string  `json:"labels,omitempty"`
+	MapFrom      []MapFrom `json:"mapFrom,omitempty"`
+}
+
+// MapFrom represents the 'mapFrom' object from variable when category
+// information is included
+type MapFrom struct{
+	SourceNames []string `json:"sourceNames,omitempty"`
+	Code        []string `json:"codes,omitempty"`
 }
