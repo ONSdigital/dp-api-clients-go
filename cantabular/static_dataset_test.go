abf8fa8429ed4eb2f303e6137b53fb1d7544bb21
Joshua Medley <jmedleyx7@gmail.com>
2021-08-19 12:48:16 +0100

    add unit test for graphql error

--- cantabular/static_dataset_test.go
+++ cantabular/static_dataset_test.go
@@ -2,6 +2,7 @@ package cantabular_test
 
 import (
 	"context"
+	"errors"
 	"testing"
 	"net/http"
 
@@ -68,6 +69,40 @@ func TestStaticDatasetQueryUnHappy(t *testing.T) {
 			Convey("Status Code 503 Service Unavailable should be recoverable from error", func() {
 				_, err := cantabularClient.StaticDatasetQuery(testCtx, req)
 				So(dperrors.StatusCode(err), ShouldEqual, http.StatusServiceUnavailable)
+			})
+		})
+	})
+
+	Convey("Given a GraphQL error from the /graphql endpoint", t, func() {
+		testCtx := context.Background()
+
+		mockHttpClient := &dphttp.ClienterMock{}
+		mockGQLClient := &mock.GraphQLClientMock{
+			QueryFunc: func(ctx context.Context, query interface{}, vars map[string]interface{}) error {
+				if q, ok := query.(*cantabular.StaticDatasetQuery); ok {
+					q.Dataset.Table.Error = "I am error response"
+					return nil
+				}
+				return errors.New("query could not be cast to correct type")
+			},
+		}
+
+		cantabularClient := cantabular.NewClient(
+			cantabular.Config{
+				Host:       "cantabular.host",
+				ExtApiHost: "cantabular.ext.host",
+			},
+			mockHttpClient,
+			mockGQLClient,
+		)
+
+		Convey("When the StaticDatasetQuery method is called", func() {
+			req := cantabular.StaticDatasetQueryRequest{}
+			_, err := cantabularClient.StaticDatasetQuery(testCtx, req)
+			
+			Convey("An error should be returned with status code 400 Bad Request", func() {
+				So(err, ShouldNotBeNil)
+				So(dperrors.StatusCode(err), ShouldEqual, http.StatusBadRequest)
 			})
 		})
 	})
