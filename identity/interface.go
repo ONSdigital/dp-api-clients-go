31f5681ae66299cb6f75f8cbb09e4cd12bdb104b
Mike Kelly <mike.kelly@methods.co.uk>
2021-12-10 15:43:51 +0000

    Add new identity mocking.

---
+++ identity/interface.go
@@ -0,0 +1,14 @@
+package identity
+
+//go:generate moq -out mock/token_identity.go . TokenIdentity
+
+import (
+	"context"
+
+	dprequest "github.com/ONSdigital/dp-net/request"
+)
+
+// TokenIdentity is the Client used by the GraphQL package to make queries
+type TokenIdentity interface {
+	CheckTokenIdentity(ctx context.Context, token string, tokenType TokenType) (*dprequest.IdentityResponse, error)
+}
