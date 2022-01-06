e115941718364d72ad0579d3db96c6027dbb2c5a
Mariana Silva <Marianasilvago1234@gmail.com>
2022-01-06 20:05:54 +0530

    update geometricData type

--- areas/data.go
+++ areas/data.go
@@ -2,12 +2,12 @@ package areas
 
 // AreaDetails represents a response area model from the areas api
 type AreaDetails struct {
-	Code          string                              `json:"code,omitempty"`
-	Name          string                              `json:"name,omitempty"`
-	DateStarted   string                              `json:"date_start,omitempty"`
-	DateEnd       string                              `json:"date_end,omitempty"`
-	WelshName     string                              `json:"name_welsh,omitempty"`
-	GeometricData []map[string]map[string]interface{} `json:"features"`
-	Visible       bool                                `json:"visible,omitempty"`
-	AreaType      string                              `json:"area_type",omitempty"`
+	Code          string                 `json:"code,omitempty"`
+	Name          string                 `json:"name,omitempty"`
+	DateStarted   string                 `json:"date_start,omitempty"`
+	DateEnd       string                 `json:"date_end,omitempty"`
+	WelshName     string                 `json:"name_welsh,omitempty"`
+	GeometricData map[string]interface{} `json:"geometry"`
+	Visible       bool                   `json:"visible,omitempty"`
+	AreaType      string                 `json:"area_type",omitempty"`
 }
