55e8019d0ddac855a81fd2c33b6962ca444e2c7d
joegasewicz <joegasewicz@gmail.com>
2022-01-27 14:37:30 +0000

    first draft

--- areas/areas.go
+++ areas/areas.go
@@ -66,10 +66,8 @@ func (c *Client) Checker(ctx context.Context, check *health.CheckState) error {
 
 // GetArea returns area information for a given area ID
 func (c *Client) GetArea(ctx context.Context, userAuthToken, serviceAuthToken, collectionID, areaID, acceptLang string) (areaDetails AreaDetails, err error) {
-	uri := fmt.Sprintf("%s/v1/areas/%s", c.hcCli.URL, areaID)
-
+	uri := fmt.Sprintf("http://%s/v1/areas/%s", c.hcCli.URL, areaID)
 	clientlog.Do(ctx, "retrieving area", service, uri)
-
 	resp, err := c.doGetWithAuthHeaders(ctx, userAuthToken, serviceAuthToken, collectionID, uri, nil, "", acceptLang)
 	if err != nil {
 		return
@@ -107,10 +105,10 @@ func (c *Client) GetArea(ctx context.Context, userAuthToken, serviceAuthToken, c
 
 // GetRelations gets the child areas
 func (c *Client) GetRelations(ctx context.Context, userAuthToken, serviceAuthToken, collectionID, areaID, acceptLang string) (relations []Relation, err error) {
-	uri := fmt.Sprintf("%s/v1/areas/%s/relations", c.hcCli.URL, areaID)
+	uri := fmt.Sprintf("http://%s/v1/areas/%s/relations", c.hcCli.URL, areaID)
 	clientlog.Do(ctx, "retrieving area relations", service, uri)
 	// Do request
-	res, err := c.doGetWithAuthHeaders(ctx, userAuthToken, serviceAuthToken, collectionID, uri, nil, "", acceptLang)
+	res, err := c.doGetWithAuthHeaders(ctx, userAuthToken, serviceAuthToken, collectionID, uri, nil, "", "en")
 	if err != nil {
 		return
 	}
@@ -125,12 +123,10 @@ func (c *Client) GetRelations(ctx context.Context, userAuthToken, serviceAuthTok
 	if err != nil {
 		return
 	}
-
-	var body map[string]interface{}
+	var body []Relation
 	if err = json.Unmarshal(b, &body); err != nil {
 		return
 	}
-
 	err = json.Unmarshal(b, &relations)
 	return
 }
