83fd8328aca381f03c467cae48a11eab2b33fefa
nimitsarup <nimitsarup@gmail.com>
2022-03-03 08:23:42 +0000

    metadata

--- interactives/data.go
+++ interactives/data.go
@@ -1,9 +1,25 @@
 package interactives
 
+import "time"
+
+type InteractiveMetadata struct { // TODO : Geography
+	Title           string    `bson:"title"                      json:"title"`
+	PrimaryTopic    string    `bson:"primary_topic"              json:"primary_topic"`
+	Topics          []string  `bson:"topics"                     json:"topics"`
+	Surveys         []string  `bson:"surveys"                    json:"surveys"`
+	ReleaseDate     time.Time `bson:"release_date"               json:"release_date"`
+	Uri             string    `bson:"uri"                        json:"uri"`
+	Edition         string    `bson:"edition,omitempty"          json:"edition,omitempty"`
+	Keywords        []string  `bson:"keywords,omitempty"         json:"keywords,omitempty"`
+	MetaDescription string    `bson:"meta_description,omitempty" json:"meta_description,omitempty"`
+	Source          string    `bson:"source,omitempty"           json:"source,omitempty"`
+	Summary         string    `bson:"summary,omitempty"          json:"summary,omitempty"`
+}
+
 type Interactive struct {
-	ID       string              `json:"id,omitempty"`
-	Metadata map[string]string   `json:"metadata,omitempty"`
-	Archive  *InteractiveArchive `json:"archive,omitempty"`
+	ID       string               `json:"id,omitempty"`
+	Metadata *InteractiveMetadata `json:"metadata,omitempty"`
+	Archive  *InteractiveArchive  `json:"archive,omitempty"`
 }
 
 type InteractiveArchive struct {
