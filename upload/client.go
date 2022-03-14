c28ae00245738dd5ebe47065b87e8070eb348caf
Dan Weinberg <dan.weinberg@armakuni.com>
2022-03-14 17:18:38 +0000

    Add upload method to upload client

--- upload/client.go
+++ upload/client.go
@@ -1,12 +1,31 @@
 package upload
 
 import (
+	"bytes"
 	"context"
+	"fmt"
 	healthcheck "github.com/ONSdigital/dp-api-clients-go/v2/health"
 	health "github.com/ONSdigital/dp-healthcheck/healthcheck"
+	dphttp "github.com/ONSdigital/dp-net/http"
+	"io"
+	"mime/multipart"
+	"net/http"
+	"strconv"
 )
 
 const service = "upload-api"
+
+type Metadata struct {
+	CollectionID  string
+	FileName      string
+	Path          string
+	IsPublishable bool
+	Title         string
+	FileSizeBytes int64
+	FileType      string
+	License       string
+	LicenseURL    string
+}
 
 // Client is an upload API client which can be used to make requests to the server.
 // It extends the generic healthcheck Client structure.
@@ -24,4 +43,37 @@ func NewAPIClient(uploadAPIURL string) *Client {
 // Checker calls image api health endpoint and returns a check object to the caller.
 func (c *Client) Checker(ctx context.Context, check *health.CheckState) error {
 	return c.hcCli.Checker(ctx, check)
+}
+
+func (c *Client) Upload(ctx context.Context, fileContent io.ReadCloser, metadata Metadata) error {
+	buff := &bytes.Buffer{}
+	formWriter := multipart.NewWriter(buff)
+	formWriter.WriteField("collectionId", metadata.CollectionID)
+	formWriter.WriteField("resumableFilename", metadata.FileName)
+	formWriter.WriteField("path", metadata.Path)
+	formWriter.WriteField("isPublishable", strconv.FormatBool(metadata.IsPublishable))
+	formWriter.WriteField("title", metadata.Title)
+	formWriter.WriteField("resumableTotalSize", fmt.Sprintf("%d", metadata.FileSizeBytes))
+	formWriter.WriteField("resumableType", metadata.FileType)
+	formWriter.WriteField("licence", metadata.License)
+	formWriter.WriteField("licenceURL", metadata.LicenseURL)
+	formWriter.WriteField("resumableChunkNumber", "1")
+	formWriter.WriteField("resumableTotalChunks", "1")
+
+	part, _ := formWriter.CreateFormFile("file", metadata.FileName)
+
+	fileContentBytes := make([]byte, metadata.FileSizeBytes)
+	fileContent.Read(fileContentBytes)
+
+	part.Write(fileContentBytes)
+
+	formWriter.Close()
+
+	defaultClient := dphttp.DefaultClient
+	req, _ := http.NewRequest(http.MethodPost, fmt.Sprintf("%s/upload", c.hcCli.URL), buff)
+	req.Header.Set("Content-Type", formWriter.FormDataContentType())
+
+	defaultClient.Do(ctx, req)
+
+	return nil
 }
--- upload/client_test.go
+++ upload/client_test.go
@@ -2,14 +2,44 @@ package upload_test
 
 import (
 	"context"
+	"fmt"
 	"github.com/ONSdigital/dp-api-clients-go/v2/health"
 	"github.com/ONSdigital/dp-api-clients-go/v2/upload"
 	"github.com/ONSdigital/dp-healthcheck/healthcheck"
 	. "github.com/smartystreets/goconvey/convey"
+	"io"
 	"net/http"
 	"net/http/httptest"
+	"strconv"
+	"strings"
 	"testing"
 	"time"
+)
+
+var (
+	actualContent, actualCollectionId string
+	actualResumableFilename           string
+	actualPath                        string
+	actualIsPublishable               string
+	actualTitle                       string
+	actualResumableTotalSize          string
+	actualResumableType               string
+	actualLicence                     string
+	actualLicenceURL                  string
+	actualResumableChunkNumber        string
+	actualResumableTotalChunks        string
+	actualMethod                      string
+)
+
+const (
+	collectionID  = "123456"
+	filename      = "file.txt"
+	path          = "data/file.txt"
+	isPublishable = false
+	title         = "Information about shoe size"
+	fileType      = "text/plain"
+	license       = "MIT"
+	licenseURL    = "https://opensource.org/licenses/MIT"
 )
 
 func TestHealthCheck(t *testing.T) {
@@ -54,4 +84,79 @@ func TestHealthCheck(t *testing.T) {
 			So(*state.LastFailure(), ShouldHappenAfter, timePriorHealthCheck)
 		})
 	})
+}
+
+func TestUpload(t *testing.T) {
+	Convey("Given the client uploads a single chunk file", t, func() {
+		fileContent := "testing"
+		f := io.NopCloser(strings.NewReader(fileContent))
+
+		Convey("And the file belongs to a collection", func() {
+
+			metadata := upload.Metadata{
+				CollectionID:  collectionID,
+				FileName:      filename,
+				Path:          path,
+				IsPublishable: isPublishable,
+				Title:         title,
+				FileSizeBytes: int64(len(fileContent)),
+				FileType:      fileType,
+				License:       license,
+				LicenseURL:    licenseURL,
+			}
+
+			Convey("When the upload is successful", func() {
+				ctx := context.Background()
+
+				s := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+					extractFields(r)
+
+					w.WriteHeader(http.StatusCreated)
+				},
+				))
+				c := upload.NewAPIClient(s.URL)
+
+				err := c.Upload(ctx, f, metadata)
+
+				So(err, ShouldBeNil)
+				So(actualMethod, ShouldEqual, http.MethodPost)
+
+				So(actualCollectionId, ShouldEqual, collectionID)
+				So(actualResumableFilename, ShouldEqual, filename)
+				So(actualPath, ShouldEqual, path)
+				So(actualIsPublishable, ShouldEqual, strconv.FormatBool(isPublishable))
+				So(actualTitle, ShouldEqual, title)
+				So(actualResumableTotalSize, ShouldEqual, fmt.Sprintf("%d", len(fileContent)))
+				So(actualResumableType, ShouldEqual, fileType)
+				So(actualLicence, ShouldEqual, license)
+				So(actualLicenceURL, ShouldEqual, licenseURL)
+				So(actualResumableChunkNumber, ShouldEqual, "1")
+				So(actualResumableTotalChunks, ShouldEqual, "1")
+
+				So(actualContent, ShouldEqual, fileContent)
+			})
+		})
+
+	})
+}
+
+func extractFields(r *http.Request) {
+	r.ParseMultipartForm(4)
+
+	actualCollectionId = r.Form.Get("collectionId")
+	actualResumableFilename = r.Form.Get("resumableFilename")
+	actualPath = r.Form.Get("path")
+	actualIsPublishable = r.Form.Get("isPublishable")
+	actualTitle = r.Form.Get("title")
+	actualResumableTotalSize = r.Form.Get("resumableTotalSize")
+	actualResumableType = r.Form.Get("resumableType")
+	actualLicence = r.Form.Get("licence")
+	actualLicenceURL = r.Form.Get("licenceURL")
+	actualResumableChunkNumber = r.Form.Get("resumableChunkNumber")
+	actualResumableTotalChunks = r.Form.Get("resumableTotalChunks")
+	actualMethod = r.Method
+
+	content, _, _ := r.FormFile("file")
+	by, _ := io.ReadAll(content)
+	actualContent = string(by)
 }
