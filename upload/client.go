a3e9a2e4dccd0549bc0958ad3d09ccde9c6bb77f
Dan Weinberg <dan.weinberg@armakuni.com>
2022-03-16 16:05:02 +0000

    Refactor upload method

--- upload/client.go
+++ upload/client.go
@@ -63,70 +63,22 @@ func (c *Client) Checker(ctx context.Context, check *health.CheckState) error {
 	return c.hcCli.Checker(ctx, check)
 }
 
-func (c *Client) writeMetadataFormFields(formWriter *multipart.Writer, metadata Metadata, chunk ChunkContext) {
-	if metadata.CollectionID != nil {
-		formWriter.WriteField("collectionId", *metadata.CollectionID)
-	}
-	formWriter.WriteField("resumableFilename", metadata.FileName)
-	formWriter.WriteField("path", metadata.Path)
-	formWriter.WriteField("isPublishable", strconv.FormatBool(metadata.IsPublishable))
-	formWriter.WriteField("title", metadata.Title)
-	formWriter.WriteField("resumableTotalSize", fmt.Sprintf("%d", metadata.FileSizeBytes))
-	formWriter.WriteField("resumableType", metadata.FileType)
-	formWriter.WriteField("licence", metadata.License)
-	formWriter.WriteField("licenceURL", metadata.LicenseURL)
-	formWriter.WriteField("resumableChunkNumber", fmt.Sprintf("%d", chunk.Current))
-	formWriter.WriteField("resumableTotalChunks", fmt.Sprintf("%d", chunk.Total))
-}
-
 func (c *Client) Upload(ctx context.Context, fileContent io.ReadCloser, metadata Metadata) error {
-	if metadata.FileSizeBytes > MaxFileSize {
-		return ErrFileTooLarge
+	if err := c.validateMetadata(metadata); err != nil {
+		return err
 	}
 
-	totalChunks := int(math.Ceil(float64(metadata.FileSizeBytes) / chunkSize))
+	totalChunks := c.calculateTotalChunks(metadata)
 
 	for i := 1; i <= totalChunks; i++ {
-		reqBuff := &bytes.Buffer{}
-		chunkContext := ChunkContext{
-			Current: i,
-			Total:   totalChunks,
-		}
-		formWriter := multipart.NewWriter(reqBuff)
-
-		readBuff := make([]byte, chunkSize)
-		bytesRead, err := fileContent.Read(readBuff)
-
+		chunkContext := ChunkContext{i, totalChunks}
+		reqBody, contentType, err := c.generateRequestBody(ctx, chunkContext, fileContent, metadata)
 		if err != nil {
-			log.Error(ctx, "file content read error", err)
-			formWriter.Close()
 			return err
 		}
 
-		var outBuff []byte
-
-		if bytesRead == chunkSize {
-			outBuff = readBuff
-		} else if bytesRead < chunkSize {
-			outBuff = readBuff[:bytesRead]
-		}
-
-		br := bytes.NewReader(outBuff)
-
-		c.writeMetadataFormFields(formWriter, metadata, chunkContext)
-		_, err = c.writeFileFormField(formWriter, metadata, br, len(outBuff))
-
-		if err != nil {
-			log.Error(ctx, "error writing form file content to request buffer", err)
-			formWriter.Close()
-			return err
-		}
-
-		formWriter.Close()
-
-		req, _ := http.NewRequest(http.MethodPost, fmt.Sprintf("%s/upload", c.hcCli.URL), reqBuff)
-
-		req.Header.Set("Content-Type", formWriter.FormDataContentType())
+		req, _ := http.NewRequest(http.MethodPost, fmt.Sprintf("%s/upload", c.hcCli.URL), reqBody)
+		req.Header.Set("Content-Type", contentType)
 
 		resp, err := dphttp.DefaultClient.Do(ctx, req)
 		if err != nil {
@@ -143,6 +95,75 @@ func (c *Client) Upload(ctx context.Context, fileContent io.ReadCloser, metadata
 	}
 
 	return nil
+}
+
+func (c *Client) writeMetadataFormFields(formWriter *multipart.Writer, metadata Metadata, chunk ChunkContext) {
+	if metadata.CollectionID != nil {
+		formWriter.WriteField("collectionId", *metadata.CollectionID)
+	}
+	formWriter.WriteField("resumableFilename", metadata.FileName)
+	formWriter.WriteField("path", metadata.Path)
+	formWriter.WriteField("isPublishable", strconv.FormatBool(metadata.IsPublishable))
+	formWriter.WriteField("title", metadata.Title)
+	formWriter.WriteField("resumableTotalSize", fmt.Sprintf("%d", metadata.FileSizeBytes))
+	formWriter.WriteField("resumableType", metadata.FileType)
+	formWriter.WriteField("licence", metadata.License)
+	formWriter.WriteField("licenceURL", metadata.LicenseURL)
+	formWriter.WriteField("resumableChunkNumber", fmt.Sprintf("%d", chunk.Current))
+	formWriter.WriteField("resumableTotalChunks", fmt.Sprintf("%d", chunk.Total))
+}
+
+func (c *Client) validateMetadata(metadata Metadata) error {
+	if metadata.FileSizeBytes > MaxFileSize {
+		return ErrFileTooLarge
+	}
+
+	return nil
+}
+
+func (c *Client) chunkReader(ctx context.Context, fileContent io.ReadCloser) (io.Reader, int, error) {
+	readBuff := make([]byte, chunkSize)
+	bytesRead, err := fileContent.Read(readBuff)
+
+	if err != nil {
+		log.Error(ctx, "file content read error", err)
+		return nil, 0, err
+	}
+
+	var outBuff []byte
+
+	if bytesRead == chunkSize {
+		outBuff = readBuff
+	} else if bytesRead < chunkSize {
+		outBuff = readBuff[:bytesRead]
+	}
+
+	return bytes.NewReader(outBuff), len(outBuff), nil
+}
+
+func (c *Client) generateRequestBody(ctx context.Context, chunkContext ChunkContext, fileContent io.ReadCloser, metadata Metadata) (*bytes.Buffer, string, error) {
+	reqBuff := &bytes.Buffer{}
+	formWriter := multipart.NewWriter(reqBuff)
+	defer formWriter.Close()
+
+	contentChunk, contentChunkLength, err := c.chunkReader(ctx, fileContent)
+	if err != nil {
+		return nil, "", err
+	}
+
+	c.writeMetadataFormFields(formWriter, metadata, chunkContext)
+	_, err = c.writeFileFormField(formWriter, metadata, contentChunk, contentChunkLength)
+	if err != nil {
+		log.Error(ctx, "error writing form file content to request buffer", err)
+
+		return nil, "", err
+	}
+
+	return reqBuff, formWriter.FormDataContentType(), nil
+}
+
+func (c *Client) calculateTotalChunks(metadata Metadata) int {
+	return int(math.Ceil(float64(metadata.FileSizeBytes) / chunkSize))
 }
 
 func (c *Client) writeFileFormField(formWriter *multipart.Writer, metadata Metadata, fileContent io.Reader, fileSizeBytes int) (int, error) {
