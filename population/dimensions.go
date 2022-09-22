ff9fc53e487206e87f8542b1b6875b979ec6e863
Joshua Medley <jmedleyx7@gmail.com>
2022-09-22 18:30:08 +0100

    add GetCategorisations func

--- population/dimensions.go
+++ population/dimensions.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"net/http"
 	"net/url"
+	"strconv"
 
 	"github.com/ONSdigital/dp-api-clients-go/v2/clientlog"
 	dperrors "github.com/ONSdigital/dp-api-clients-go/v2/errors"
@@ -27,23 +28,38 @@ type GetDimensionsInput struct {
 	SearchString   string
 }
 
+type GetCategorisationsInput struct {
+	AuthTokens
+	PaginationParams
+	PopulationType string
+	Dimension      string
+}
+
 // GetDimensionsResponse is the response object for GetDimensions
 type GetDimensionsResponse struct {
 	PaginationResponse
 	Dimensions []Dimension `json:"items"`
 }
 
+type GetCategorisationsResponse struct {
+	PaginationResponse
+	Items []Dimension `json:"items"`
+}
+
 func (c *Client) GetDimensions(ctx context.Context, input GetDimensionsInput) (GetDimensionsResponse, error) {
 	logData := log.Data{
-		"method":         http.MethodGet,
-		"limit":          input.Limit,
-		"offset":         input.Offset,
-		"populationType": input.PopulationType,
-		"search string":  input.SearchString,
+		"method":          http.MethodGet,
+		"limit":           input.Limit,
+		"offset":          input.Offset,
+		"population_type": input.PopulationType,
+		"search_string":   input.SearchString,
 	}
 
 	urlPath := fmt.Sprintf("/population-types/%s/dimensions", input.PopulationType)
-	var urlValues map[string][]string
+	urlValues := url.Values{
+		"limit":  []string{strconv.Itoa(input.Limit)},
+		"offset": []string{strconv.Itoa(input.Offset)},
+	}
 	if input.SearchString != "" {
 		urlValues = url.Values{"q": []string{input.SearchString}}
 	}
@@ -88,4 +104,61 @@ func (c *Client) GetDimensions(ctx context.Context, input GetDimensionsInput) (G
 	}
 
 	return dimensions, nil
+}
+
+func (c *Client) GetCategorisations(ctx context.Context, input GetCategorisationsInput) (GetCategorisationsResponse, error) {
+	logData := log.Data{
+		"method":          http.MethodGet,
+		"limit":           input.Limit,
+		"offset":          input.Offset,
+		"population_type": input.PopulationType,
+		"dimension":       input.Dimension,
+	}
+
+	urlPath := fmt.Sprintf("/population-types/%s/dimensions/categorisations", input.PopulationType)
+	urlValues := url.Values{
+		"limit":  []string{strconv.Itoa(input.Limit)},
+		"offset": []string{strconv.Itoa(input.Offset)},
+	}
+
+	req, err := c.createGetRequest(ctx, input.UserAuthToken, input.ServiceAuthToken, urlPath, urlValues)
+	if err != nil {
+		return GetCategorisationsResponse{}, dperrors.New(
+			err,
+			dperrors.StatusCode(err),
+			logData,
+		)
+	}
+
+	clientlog.Do(ctx, "getting dimension categorisations", service, req.URL.String(), logData)
+
+	res, err := c.hcCli.Client.Do(ctx, req)
+	if err != nil {
+		return GetCategorisationsResponse{}, dperrors.New(
+			errors.Wrap(err, "failed to get response from Population types API"),
+			http.StatusInternalServerError,
+			logData,
+		)
+	}
+
+	defer func() {
+		if err := res.Body.Close(); err != nil {
+			log.Error(ctx, "error closing http response body", err)
+		}
+	}()
+
+	if err := checkGetResponse(res); err != nil {
+		return GetCategorisationsResponse{}, err
+	}
+
+	var resp GetCategorisationsResponse
+	if err := json.NewDecoder(res.Body).Decode(&resp); err != nil {
+		return GetCategorisationsResponse{}, dperrors.New(
+			errors.Wrap(err, "unable to deserialize categorisations response"),
+			http.StatusInternalServerError,
+			logData,
+		)
+	}
+
+	return resp, nil
 }
