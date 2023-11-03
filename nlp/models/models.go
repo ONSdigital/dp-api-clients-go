c78d5e7a0fccb365344865e51990b39c8abd79f3
kamen.dimitrov1 <kamen.dimitrov@flaxandteal.co.uk>
2023-11-03 10:18:37 +0200

    sorted the models file by first letters

--- nlp/models/models.go
+++ nlp/models/models.go
@@ -1,15 +1,34 @@
 package models
 
 type NLPResp struct {
-	Scrubber Scrubber
-	Category Category
 	Berlin   Berlin
+	Category Category
+	Scrubber Scrubber
+}
+
+type Berlin struct {
+	Matches []Matches `json:"matches,omitempty"`
+}
+
+type Matches struct {
+	Codes       []string `json:"codes,omitempty"`
+	Encoding    string   `json:"encoding,omitempty"`
+	Names       []string `json:"names,omitempty"`
+	ID          string   `json:"id,omitempty"`
+	Key         string   `json:"key,omitempty"`
+	State       []string `json:"state,omitempty"`
+	Subdivision []string `json:"subdiv,omitempty"`
+	Words       []string `json:"words,omitempty"`
+}
+type Category []struct {
+	Code  []string `json:"c,omitempty"`
+	Score float64  `json:"s,omitempty"`
 }
 
 type Scrubber struct {
-	Query   string  `json:"query"`
 	Results Results `json:"results,omitempty"`
 	Time    string  `json:"time,omitempty"`
+	Query   string  `json:"query"`
 }
 
 type Results struct {
@@ -27,24 +46,4 @@ type AreaResp struct {
 type IndustryResp struct {
 	Code string `json:"code,omitempty"`
 	Name string `json:"name,omitempty"`
-}
-
-type Category []struct {
-	Code  []string `json:"c,omitempty"`
-	Score float64  `json:"s,omitempty"`
-}
-
-type Berlin struct {
-	Matches []Matches `json:"matches,omitempty"`
-}
-
-type Matches struct {
-	Codes       []string `json:"codes,omitempty"`
-	Encoding    string   `json:"encoding,omitempty"`
-	ID          string   `json:"id,omitempty"`
-	Key         string   `json:"key,omitempty"`
-	Names       []string `json:"names,omitempty"`
-	State       []string `json:"state,omitempty"`
-	Subdivision []string `json:"subdiv,omitempty"`
-	Words       []string `json:"words,omitempty"`
 }
